52f1e294768db420fd2b74476ad2cbf86da5992c
bob-sheehy <bob_sheehy@hotmail.com>
2014-03-30 22:10:16 +0100

    second committ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+AdminUser.create([{ name: 'test', password: 'secret', password_confirmation: 'secret' }])
